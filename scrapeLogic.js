e90adb00b4a486f64167d66f19c6e10234186f7a
Dmitriy Blashchyk <dmytro.blashchyk@gmail.com>
2023-09-20 13:48:45 +0300

    fix errors

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -9,34 +9,32 @@ const scrapeLogic = async (postData, res) => {
       projectId: postData.projectId,
       period: postData.period,
     })
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+          process.env.NODE_ENV === "production"
+              ? process.env.PUPPETEER_EXECUTABLE_PATH
+              : puppeteer.executablePath(),
+    });
+    try {
+      const page = await browser.newPage();
+
+      await page.setContent(request.data)
+
+      return await page.pdf();
+    } catch (e) {
+      console.error(e);
+      res.send(`Something went wrong while running Puppeteer: ${e}`);
+    } finally {
+      await browser.close();
+    }
   } catch (e) {
     console.log('!!!!!', e);
-  }
-
-
-  const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-  });
-  try {
-    const page = await browser.newPage();
-
-    await page.setContent(request.data)
-
-    return await page.pdf();
-  } catch (e) {
-    console.error(e);
-    res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
-    await browser.close();
   }
 };
 
